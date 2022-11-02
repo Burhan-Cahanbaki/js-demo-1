041b23f8fc0c1c91de2f1b40140d94bbab89b5a0
Burhan Habibul <burhaniddin123@gmail.com>
2022-11-02 21:41:23 +0300

    Second change

---
+++ hello-world.js
@@ -0,0 +1,35 @@
+//Merhaba Dunya
+
+//console.log("Hello World");
+//document.write("Merhaba Dunya");
+
+var servername="Burhan cahanbaki"
+console.log(servername)
+
+let burhan;
+burhan="Habul"
+console.log(burhan)
+let password="1345";
+console.log(password)
+
+let fullname="burhan cahanbaki ";
+console.log(fullname)
+fullname="Mustafa Cahanbaki"
+console.log(fullname)
+fullname+"Yeni eklenen bulgi"
+console.log(fullname +" test bilgisi")
+fullname=fullname+"test bilgileri"
+console.log(fullname)
+
+fullname="sifirladik"
+fullname+="yeni bilgi eklendi"
+console.log(fullname)
+const server="1243345"
+console.log(server)
+
+
+
+
+
+
+
